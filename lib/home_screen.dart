d92ccea3735bf052230fbdf01e2c2e03d7bde9a1
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2023-03-09 12:02:55 +0530

    state-management

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'getx_routes /screen_one.dart';
+import 'state_management_example /counter_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,14 +12,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
+final CounterController controller = Get.put(CounterController());
   @override
+  void initState(){
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Getx App'),
       ),
-      body:
+      body:Center(
+        child: Obx(()=>Text(controller.counter.toString(),style: TextStyle(fontSize: 60),)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          controller.increment();
+        },
+      ),
     );
   }
 }
---
+++ lib/state_management_example /counter_screen.dart
@@ -0,0 +1,9 @@
+
+import 'package:get/get.dart';
+
+class CounterController extends GetxController{
+  RxInt counter = 1.obs;
+  increment(){
+    counter.value++;
+  }
+}
